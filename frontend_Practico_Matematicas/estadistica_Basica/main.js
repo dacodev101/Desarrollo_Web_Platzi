9c5d6ecee2da379fd06f57158ba9a663144637fb
Daniel Ballen <daniel199728@hotmail.com>
2023-11-17 12:41:07 -0500

    Agregar Logica Frontend Estadistica Basica

---
+++ frontend_Practico_Matematicas/estadistica_Basica/main.js
@@ -0,0 +1,90 @@
+const inputArray = document.querySelector("#input-list");
+const inputOrdenado = document.querySelector(".input-ordenado");
+const resultadoOperacion = document.querySelector(".result-tendencia");
+const botonPromedio = document.querySelector(".promedio");
+const botonMediana = document.querySelector(".mediana");
+const botonModa = document.querySelector(".moda");
+
+botonPromedio.addEventListener("click", () => calcularPromedio(convertirInputANumerosYTexto(inputArray))
+);
+botonMediana.addEventListener("click", () =>
+    calcularMediana(convertirInputANumerosYTexto(inputArray))
+);
+botonModa.addEventListener("click", () => calcularModa(convertirInputANumerosYTexto(inputArray)));
+
+function calcularPromedio(array) {
+  // function sumarElementos (valorAcumulado, nuevoValor){
+  //     return valorAcumulado + nuevoValor
+  // }
+  const sumaLista = array.reduce((acumulador, actual) => {
+    console.log(acumulador, actual);
+    return acumulador + actual;
+  });
+  resultado =  Math.round(((sumaLista / array.length) * 100)) / 100 ;
+  resultadoOperacion.innerText = `Promedio == ${resultado} `;
+}
+
+function esPar(Lista) {
+  return Lista.length % 2 ? false : true;
+}
+
+function ordenarLista(lista) {
+  return lista.sort((a, b) => a - b);
+}
+
+function ordenarListaBidimensional(lista) {
+  return lista.sort((a, b) => a[1] - b[2]);
+}
+
+function calcularMediana(listaMediana) {
+  const lista = ordenarLista(listaMediana);
+  const listaEsPar = esPar(lista);
+
+  if (listaEsPar) {
+    const indexMitadLista = Math.round(lista.length / 2);
+    const sumaMitades =
+      (lista[indexMitadLista] + lista[indexMitadLista - 1]) / 2;
+    resultadoOperacion.innerText = `Mediana ${sumaMitades} `;
+  } else {
+    const indexMitadListaImpar = Math.floor(lista.length / 2);
+    resultadoOperacion.innerText = `Mediana == ${lista[indexMitadListaImpar]}`;
+  }
+}
+
+function calcularModa(lista) {
+  lista.sort();
+
+  const objDatos = {};
+  for (const dato of lista) {
+    objDatos[dato] ? (objDatos[dato] += 1) : (objDatos[dato] = 1);
+  }
+
+  const mayorFrecuencia = Math.max(...Object.values(objDatos));
+  const modas = [];
+
+  for (const [valor, frecuencia] of Object.entries(objDatos)) {
+    console.log(valor, frecuencia);
+    if (frecuencia === mayorFrecuencia) {
+      modas.push(valor);
+    }
+  }
+  resultadoOperacion.innerText = `Moda == ${
+    modas.length === 1 ? modas[0] : modas
+  }`;
+}
+
+function convertirInputANumerosYTexto(input) {
+    const valores = input.value.split(" ");
+    const arrayNumeros = valores.map((valor) => {
+        if (isNaN(valor)) {
+            return valor;
+        } else {
+            return Number(valor);
+        }
+    });
+    const arrayTexto = valores.filter((valor) => isNaN(valor));
+    const arrayNumerosTexto = ordenarLista([...arrayNumeros, ...arrayTexto])
+    inputOrdenado.innerText = arrayNumerosTexto.join(" ")
+    inputOrdenado.classList.remove('inactive')
+    return arrayNumerosTexto;
+}
